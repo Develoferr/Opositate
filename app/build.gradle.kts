d8618d7982f1940f038892879938854faee840ac
Fer <programacionfer@gmail.com>
2024-10-10 23:51:04 +0200

    Add Firebase BoM, Google Services dependencies and Google Services plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.hilt)
-//    alias(libs.plugins.google.services)
+    alias(libs.plugins.google.services)
 }
 
 android {
