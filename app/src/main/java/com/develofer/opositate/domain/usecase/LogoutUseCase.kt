048002f282b3217fc33e4ef7e8d6e6b5da51e1ff
Fer <programacionfer@gmail.com>
2024-10-13 00:27:21 +0200

    Rename LogoutUseCase and add @Singleton annotation for dependency management

--- app/src/main/java/com/develofer/opositate/domain/usecase/LogoutUseCase.kt
+++ app/src/main/java/com/develofer/opositate/domain/usecase/LogoutUseCase.kt
@@ -2,7 +2,9 @@ package com.develofer.opositate.domain.usecase
 
 import com.develofer.opositate.domain.repository.AuthRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class LogoutUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
