d067de537fb93ed83c8635edf657b2fe9d3e2edb
Fer <programacionfer@gmail.com>
2024-10-19 11:26:00 +0200

    Refactor CustomLoginTextField for improved readability and organization
    - Grouped related logic and separated UI components into helper functions
    - Improved code consistency with better spacing and formatting
    - Simplified inline label and supporting text components
    - Enhanced naming conventions for clarity
    - Optimized trailing icon logic for password and tooltip handling

--- app/src/main/java/com/develofer/opositate/presentation/custom/CustomLoginTextField.kt
+++ app/src/main/java/com/develofer/opositate/presentation/custom/CustomLoginTextField.kt
@@ -3,7 +3,6 @@ package com.develofer.opositate.presentation.custom
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -57,117 +56,38 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun CustomLoginTextField(
-    value: String,
-    onValueChange: (String) -> Unit,
-    label: String,
-    isFocused: Boolean,
-    onFocusChange: (Boolean) -> Unit,
-    isPasswordField: Boolean = false,
-    supportingText: ValidateFieldErrors,
-    isDarkTheme: Boolean,
-    textLetterSpacing: TextUnit = 2.sp,
-    labelFontSize: TextUnit = 15.sp,
-    textFieldPaddingBottom: Dp = 0.dp,
-    haveToolTip: Boolean = false,
-    toolTipText: String = "Test tooltip",
-    painter: Painter = painterResource(id = R.drawable.ic_info_bold),
+    value: String, onValueChange: (String) -> Unit, label: String, isFocused: Boolean,
+    onFocusChange: (Boolean) -> Unit, isPasswordField: Boolean = false,
+    supportingText: ValidateFieldErrors, isDarkTheme: Boolean, textLetterSpacing: TextUnit = 2.sp,
+    labelFontSize: TextUnit = 15.sp, textFieldPaddingBottom: Dp = 0.dp, haveToolTip: Boolean = false,
+    toolTipText: String = "", painter: Painter = painterResource(id = R.drawable.ic_info_bold),
 ) {
     var isPasswordVisible by remember { mutableStateOf(false) }
-    val fieldColors = getLoginFieldColors(isDarkTheme)
     var showTooltip by remember { mutableStateOf(false) }
     var tooltipPosition by remember { mutableStateOf(Offset.Zero) }
     var iconSize by remember { mutableStateOf(IntSize.Zero) }
-    val trailingIcon: @Composable (() -> Unit)? =
-        if (isPasswordField) {{
-            val padding = if (isPasswordVisible) (-4).dp else 0.dp
-            IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
-                val modifier =
-                    if (isFocused || value.isNotEmpty()) Modifier.offset(y = 8.dp)
-                    else Modifier.offset(y = 12.dp)
-                Image(
-                    painter = if (isPasswordVisible) painterResource(id = R.drawable.ic_open_eye)
-                    else painterResource(id = R.drawable.ic_closed_eye),
-                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground),
-                    contentDescription = stringResource(id = R.string.custom_login_text_field_password_eye_image_content_description),
-                    modifier = modifier
-                        .size(24.dp)
-                        .offset(y = padding)
-                )
-            }
-        }} else if (haveToolTip) {{
-            Icon(
-                painter = painter,
-                contentDescription = "Info Icon",
-                modifier = Modifier
-                    .offset(y = 8.dp).size(16.dp)
-                    .onGloballyPositioned { coordinates: LayoutCoordinates ->
-                        iconSize = coordinates.size
-                        tooltipPosition = coordinates.localToWindow(Offset.Zero)
-                    }
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onPress = {
-                                showTooltip = true
-                                delay(4500)
-                                showTooltip = false
-                            }
-                        )
-                    }
-            )
-        }} else null
 
+    val fieldColors = getLoginFieldColors(isDarkTheme)
+    val trailingIcon = getTrailingIcon(
+        isPasswordField, haveToolTip, isPasswordVisible, isFocused, value, painter,
+        { isPasswordVisible = !isPasswordVisible },
+        { coordinates -> iconSize = coordinates.size; tooltipPosition = coordinates.localToWindow(Offset.Zero) },
+        { newVisibility ->  showTooltip = newVisibility }
+    )
     TextField(
         singleLine = true,
         value = value,
         onValueChange = onValueChange,
-        label = {
-            Text(
-                maxLines = 1,
-                text = label,
-                fontSize =
-                    if (isFocused || value.isNotEmpty()) 10.sp
-                    else labelFontSize,
-                letterSpacing = 2.sp,
-                color = MaterialTheme.colorScheme.onBackground,
-                style =
-                    if (isFocused || value.isNotEmpty()) MaterialTheme.typography.labelMedium
-                    else MaterialTheme.typography.bodyLarge,
-                fontWeight = FontWeight.Light,
-                modifier =
-                    if (isFocused || value.isNotEmpty()) Modifier.offset(x = (-17).dp)
-                    else Modifier.offset(y = 13.dp, x = (-17).dp)
-            )
-        }, supportingText = {
-            val supportingTextFieldError = when (supportingText) {
-                ValidateFieldErrors.INVALID_EMAIL -> stringResource(
-                    id = R.string.login_screen__supporting_text__invalid_email
-                )
-                ValidateFieldErrors.EMPTY_TEXT -> stringResource(
-                    id = R.string.login_screen__supporting_text__empty_field
-                )
-                ValidateFieldErrors.EMAILS_DO_NOT_MATCH -> stringResource(
-                    id = R.string.resetPassword_dialog__supporting_text__emails_do_not_match
-                )
-                else -> ""
-            }
-            Text(
-                text = supportingTextFieldError,
-                fontSize = if (isSystemInDarkTheme()) 10.sp else 10.sp,
-                letterSpacing = textLetterSpacing,
-                color = MaterialTheme.colorScheme.error
-            )
-        },
+        label = { getLabel(label, isFocused, value, labelFontSize) },
+        supportingText = { GetSupportingText(supportingText, textLetterSpacing) },
         colors = TextFieldDefaults.colors(
             focusedContainerColor = fieldColors.containerColor,
             unfocusedContainerColor = fieldColors.containerColor,
-            disabledContainerColor = fieldColors.containerColor,
             focusedIndicatorColor = fieldColors.indicatorColor,
             unfocusedIndicatorColor = fieldColors.indicatorColor,
-            disabledIndicatorColor = fieldColors.indicatorColor,
             cursorColor = fieldColors.cursorColor,
             focusedTextColor = Color.Black,
-            unfocusedTextColor = Color.Black,
-            disabledTextColor = Color.Black,
+            unfocusedTextColor = Color.Black
         ),
         modifier = Modifier
             .fillMaxWidth()
@@ -175,42 +95,119 @@ fun CustomLoginTextField(
             .padding(textFieldPaddingBottom)
             .height(76.dp)
             .onFocusChanged { onFocusChange(it.isFocused) },
-        textStyle =
-            TextStyle(
-                color = MaterialTheme.colorScheme.onBackground,
-                fontWeight = FontWeight.Normal,
-                fontStyle = MaterialTheme.typography.bodyMedium.fontStyle,
-                fontSize = 16.sp,
-                letterSpacing = 1.sp
-            ),
-        visualTransformation =
-            if (isPasswordField && !isPasswordVisible) PasswordVisualTransformation()
-            else VisualTransformation.None,
-        trailingIcon = trailingIcon
-        ,
-        keyboardOptions = KeyboardOptions(
+        textStyle = TextStyle(
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            letterSpacing = 1.sp
+        ),
+        visualTransformation = if (isPasswordField && !isPasswordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions.Default.copy(
             keyboardType = KeyboardType.Email,
             imeAction = ImeAction.Next
         ),
         keyboardActions = KeyboardActions(onNext = { onValueChange("") })
     )
     if (showTooltip) {
-        Tooltip(tooltipPosition, iconSize, toolTipText = toolTipText)
+        Tooltip(tooltipPosition, iconSize, toolTipText)
     }
 }
 
+@Composable
+fun getLabel(
+    label: String, isFocused: Boolean, value: String, labelFontSize: TextUnit): @Composable () -> Unit
+{
+    return {
+        Text(
+            text = label,
+            fontSize = if (isFocused || value.isNotEmpty()) 10.sp else labelFontSize,
+            letterSpacing = 2.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Light,
+            modifier = if (isFocused || value.isNotEmpty()) Modifier.offset(x = (-17).dp) else Modifier.offset(y = 13.dp, x = (-17).dp)
+        )
+    }
+}
 
 @Composable
-fun Tooltip(position: Offset, iconSize: IntSize, toolTipText: String) {
+fun GetSupportingText(
+    supportingText: ValidateFieldErrors, letterSpacing: TextUnit
+) {
+    val supportingTextFieldError = when (supportingText) {
+        ValidateFieldErrors.INVALID_EMAIL -> stringResource(id = R.string.custom_login_text_field__supporting_text__invalid_email)
+        ValidateFieldErrors.EMPTY_TEXT -> stringResource(id = R.string.custom_login_text_field__supporting_text__empty_field)
+        ValidateFieldErrors.EMAILS_DO_NOT_MATCH -> stringResource(id = R.string.custom_login_text_field__supporting_text__emails_do_not_match)
+        else -> ""
+    }
+    Text(
+        text = supportingTextFieldError,
+        fontSize = 10.sp,
+        letterSpacing = letterSpacing,
+        color = MaterialTheme.colorScheme.error
+    )
+}
+
+@Composable
+private fun getLoginFieldColors(isDarkTheme: Boolean): LoginFieldColors {
+    val containerColor = if (isDarkTheme) Color.Black else MaterialTheme.colorScheme.primary
+    val indicatorColor = if (isDarkTheme) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground
+    val cursorColor = if (isDarkTheme) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground
+    return LoginFieldColors(containerColor, indicatorColor, cursorColor)
+}
+
+@Composable
+fun getTrailingIcon(
+    isPasswordField: Boolean, haveToolTip: Boolean, isPasswordVisible: Boolean, isFocused: Boolean,
+    value: String, painter: Painter, onPasswordToggle: () -> Unit, onGloballyPositioned: (LayoutCoordinates) -> Unit,
+    toggleToolTipVisibility: (newVisibility: Boolean) -> Unit
+): @Composable (() -> Unit)? {
+    return when {
+        isPasswordField -> {
+            val padding = if (isPasswordVisible) (-4).dp else 0.dp
+            {
+                IconButton(onClick = onPasswordToggle) {
+                    val modifier = if (isFocused || value.isNotEmpty()) Modifier.offset(y = 8.dp) else Modifier.offset(y = 12.dp)
+                    Image(
+                        painter = if (isPasswordVisible) painterResource(id = R.drawable.ic_open_eye) else painterResource(id = R.drawable.ic_closed_eye),
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground),
+                        contentDescription = stringResource(id = R.string.custom_login_text_field_password_eye_image_content_description),
+                        modifier = modifier.size(24.dp).offset(y = padding)
+                    )
+                }
+            }
+        }
+        haveToolTip -> {
+            {
+                Icon(
+                    painter = painter,
+                    contentDescription = stringResource(id = R.string.custom_login_text_field__content_description_ic_info),
+                    modifier = Modifier
+                        .offset(y = 8.dp).size(16.dp)
+                        .onGloballyPositioned(onGloballyPositioned)
+                        .pointerInput(Unit) { detectTapGestures(onPress = {
+                            toggleToolTipVisibility(true)
+                            delay(4500)
+                            toggleToolTipVisibility(false)
+                        }) }
+                )
+            }
+        }
+        else -> null
+    }
+}
+
+@Composable
+fun Tooltip(
+    position: Offset, iconSize: IntSize, toolTipText: String
+) {
     val density = LocalDensity.current
     Popup(
         alignment = Alignment.TopStart,
         offset = IntOffset((position.x.toInt()-150), (position.y.toInt() + 50))
     ) {
         Box(
-            modifier = Modifier
-                .background(Color.Gray)
-                .wrapContentSize()
+            modifier = Modifier.background(Color.Gray).wrapContentSize()
         ) {
             Text(
                 text = toolTipText,
@@ -219,15 +216,6 @@ fun Tooltip(position: Offset, iconSize: IntSize, toolTipText: String) {
             )
         }
     }
-}
-
-
-@Composable
-private fun getLoginFieldColors(isDarkTheme: Boolean): LoginFieldColors {
-    val containerColor = if (isDarkTheme) Color.Black else MaterialTheme.colorScheme.primary
-    val indicatorColor = if (isDarkTheme) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground
-    val cursorColor = if (isDarkTheme) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground
-    return LoginFieldColors(containerColor, indicatorColor, cursorColor)
 }
 
 private data class LoginFieldColors(
