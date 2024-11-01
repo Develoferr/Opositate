4a2efd0cfdcbf6cbe522ad912d7559887a68fa2c
Fer <programacionfer@gmail.com>
2024-11-01 22:10:26 +0100

    Add userId document creation in scores collection with initial scores and level on user registration

--- app/src/main/java/com/develofer/opositate/feature/login/data/AuthRepositoryImpl.kt
+++ app/src/main/java/com/develofer/opositate/feature/login/data/AuthRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.develofer.opositate.feature.login.data
 import com.develofer.opositate.feature.login.domain.repository.AuthRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,18 +14,77 @@ class AuthRepositoryImpl @Inject constructor(
 
     override fun getUser() = auth.currentUser
 
-    override fun getUserName(): String {
-        return auth.currentUser?.displayName ?: ""
-    }
+    override fun getUserName(): String = getUser()?.displayName ?: ""
 
     override fun createUser(username: String, email: String, password: String, onSuccess: () -> Unit, onFailure: (String) -> Unit) {
         if (username.isNotBlank() && email.isNotBlank() && password.isNotBlank()) {
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     updateUsername(username, onSuccess, onFailure)
-                    onSuccess()
+                    createUserScoreDocument(onSuccess, onFailure)
                 } else {
                     onFailure(task.exception?.message ?: "Registration failed")
+                }
+            }
+        }
+    }
+    private fun createUserScoreDocument(onSuccess: () -> Unit, onFailure: (String) -> Unit) {
+        val userId = getUser()?.uid
+        if (userId != null) {
+            val firestore = FirebaseFirestore.getInstance()
+            val scoresCollection = firestore.collection("scores")
+            val userScoreDocument = scoresCollection.document(userId)
+            userScoreDocument.set(
+                mapOf(
+                    "level" to 0,
+                    "scores" to listOf(
+                        mapOf(
+                            "ability" to "1",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "2",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "3",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "4",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "5",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "6",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "7",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                        mapOf(
+                            "ability" to "8",
+                            "start_score" to 0,
+                            "present_score" to 0,
+                        ),
+                    )
+                )
+            ).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(task.exception?.message ?: "Failed to create user score document")
                 }
             }
         }
