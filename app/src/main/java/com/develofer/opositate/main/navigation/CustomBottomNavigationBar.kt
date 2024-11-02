3055840c42f017f8f802e2da8de35127b424638c
Fer <programacionfer@gmail.com>
2024-11-02 22:18:05 +0100

    Implemented gradient effect on selected icons in BottomNavigationBar

--- app/src/main/java/com/develofer/opositate/main/navigation/CustomBottomNavigationBar.kt
+++ app/src/main/java/com/develofer/opositate/main/navigation/CustomBottomNavigationBar.kt
@@ -1,17 +1,21 @@
 package com.develofer.opositate.main.navigation
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.develofer.opositate.R
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.graphicsLayer
 import com.develofer.opositate.main.navigation.AppRoutes.Destination
 
 @Composable
@@ -29,10 +33,26 @@ fun CustomBottomNavigationBar(navController: NavHostController) {
             NavigationBarItem(
                 icon = {
                     when (destination) {
-                        Destination.PROFILE -> Icon(painter = painterResource(id = R.drawable.ic_brain_6), contentDescription = "", modifier = Modifier.size(24.dp))
-                        Destination.TEST -> Icon(painter = painterResource(id = R.drawable.ic_test), contentDescription = "", modifier = Modifier.size(24.dp))
-                        Destination.LESSON -> Icon(painter = painterResource(id = R.drawable.ic_lesson), contentDescription = "", modifier = Modifier.size(24.dp))
-                        Destination.CALENDAR -> Icon(painter = painterResource(id = R.drawable.ic_calendar), contentDescription = "", modifier = Modifier.size(24.dp))
+                        Destination.PROFILE -> GradientIcon(
+                            painter = painterResource(id = R.drawable.ic_brain_6),
+                            contentDescription = "",
+                            isSelected = currentRoute == destination.route
+                        )
+                        Destination.TEST -> GradientIcon(
+                            painter = painterResource(id = R.drawable.ic_test),
+                            contentDescription = "",
+                            isSelected = currentRoute == destination.route
+                        )
+                        Destination.LESSON -> GradientIcon(
+                            painter = painterResource(id = R.drawable.ic_lesson),
+                            contentDescription = "",
+                            isSelected = currentRoute == destination.route
+                        )
+                        Destination.CALENDAR -> GradientIcon(
+                            painter = painterResource(id = R.drawable.ic_calendar),
+                            contentDescription = "",
+                            isSelected = currentRoute == destination.route
+                        )
                         Destination.LOGIN -> {}
                         Destination.REGISTER -> {}
                     }
@@ -50,4 +70,45 @@ fun CustomBottomNavigationBar(navController: NavHostController) {
             )
         }
     }
+}
+
+@Composable
+fun GradientIcon(
+    painter: Painter,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    isSelected: Boolean
+) {
+    val colors = listOf(
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.secondary
+    )
+    val iconTint = if (isSelected) {
+        Modifier
+            .graphicsLayer(alpha = 0.99f)
+            .drawWithCache {
+                onDrawWithContent {
+                    drawContent()
+                    drawRect(
+                        brush = Brush.linearGradient(
+                            colors = colors,
+                            start = Offset(0f, size.height),
+                            end = Offset(size.width, 0f)
+                        ),
+                        blendMode = BlendMode.SrcAtop
+                    )
+                }
+            }
+    } else {
+        Modifier
+    }
+
+    Icon(
+        painter = painter,
+        contentDescription = contentDescription,
+        modifier = modifier
+            .then(iconTint)
+            .size(24.dp),
+        tint = if (isSelected) Color.Unspecified else LocalContentColor.current
+    )
 }
