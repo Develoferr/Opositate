bfaa61ace7344190a8dedb295d4b86cf30a28d6e
Fer <programacionfer@gmail.com>
2024-11-07 19:02:30 +0100

    Create TestResultScreen and ViewModel

---
+++ app/src/main/java/com/develofer/opositate/feature/test/TestResultViewModel.kt
@@ -0,0 +1,40 @@
+package com.develofer.opositate.feature.test
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.develofer.opositate.feature.profile.SolvedTest
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TestResultViewModel @Inject constructor(
+    private val getTestResultUseCase: GetTestResultUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(TestResultUiState())
+    val uiState: StateFlow<TestResultUiState> = _uiState.asStateFlow()
+
+    fun changeQuestion(sum: Boolean) {
+        if (sum) _uiState.update { it.copy(currentQuestionIndex = it.currentQuestionIndex + 1) }
+        else _uiState.update { it.copy(currentQuestionIndex = it.currentQuestionIndex - 1) }
+    }
+    fun getTestResult(testResultId: String) {
+        viewModelScope.launch {
+            _uiState.update { it.copy(testResult = getTestResultUseCase(testResultId)) }
+        }
+    }
+}
+
+data class TestResultUiState(
+    val currentQuestionIndex: Int = -1,
+    val timeCount: Int = 0,
+    val isTestActive: Boolean = false,
+    val showStartDialog: Boolean = true,
+    val showPauseDialog: Boolean = false,
+    val testResult: SolvedTest? = null
+)
--- app/src/main/java/com/develofer/opositate/utils/Constants.kt
+++ app/src/main/java/com/develofer/opositate/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.develofer.opositate.utils
 object Constants {
     const val EMPTY_TEXT = ""
     const val TWO_DIGITS_FORMAT = "%02d"
+    const val TWO_DECIMALS_FORMAT = "%.2f"
 }
