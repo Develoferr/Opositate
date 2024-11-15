c920229c215d7799268b1a523277345bccd29bd5
Fer <programacionfer@gmail.com>
2024-11-15 02:46:35 +0100

    Modify TestRepository to handle task-based test retrieval

--- app/src/main/java/com/develofer/opositate/feature/test/data/repository/TestRepositoryImpl.kt
+++ app/src/main/java/com/develofer/opositate/feature/test/data/repository/TestRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package com.develofer.opositate.feature.test.data.repository
 
 import com.develofer.opositate.R
-import com.develofer.opositate.feature.profile.data.model.PsTest
-import com.develofer.opositate.feature.profile.presentation.model.PsTestVO
+import com.develofer.opositate.feature.profile.presentation.model.PsTestDocumentResponse
+import com.develofer.opositate.feature.profile.presentation.model.PsTestResponse
 import com.develofer.opositate.feature.test.domain.repository.TestRepository
 import com.develofer.opositate.main.data.model.Result
 import com.develofer.opositate.main.data.provider.ResourceProvider
@@ -11,27 +11,57 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
+private const val TASKS_PER_ABILITY = 8
+
 class TestRepositoryImpl @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val resourceProvider: ResourceProvider
 ): TestRepository {
 
-    override suspend fun getTestList(): Result<List<PsTestVO>> {
+    override suspend fun getTestList(): Result<List<PsTestDocumentResponse>> {
         return try {
-            val voTestList = getTestCollection().get().await().map {
-                it.toObject(PsTestVO::class.java).apply { isEnabled = true }
+            val psTestResponseList = getTestCollection().get().await().map {
+                it.toObject(PsTestDocumentResponse::class.java)
             }
-            Result.Success(voTestList)
+            Result.Success(psTestResponseList)
         } catch (e: Exception) {
             Result.Error(e)
         }
     }
 
-    override suspend fun getTest(id: String): Result<PsTest?> {
+    override suspend fun getTestListByAbility(abilityId: Int): Result<List<PsTestDocumentResponse>> {
         return try {
-            val document = getTestCollection().document(id).get().await()
-            val test = if (document.exists()) document.toObject(PsTest::class.java) else null
-            Result.Success(test)
+            val testPerTaskList = mutableListOf<PsTestDocumentResponse>()
+            for (i in 1..TASKS_PER_ABILITY) {
+                val documentId = "${abilityId}_${i - 1}"
+                val document = getTestCollection().document(documentId).get().await()
+                val testList = if (document.exists()) document.toObject(PsTestDocumentResponse::class.java) else null
+                testList?.let { testPerTaskList.add(testList) }
+            }
+            Result.Success(testPerTaskList.toList())
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun getTestListByTask(abilityId: Int, abilityTaskId: Int): Result<List<PsTestResponse>> {
+        return try {
+            val documentId = "${abilityId}_${abilityTaskId}"
+            val document = getTestCollection().document(documentId).get().await()
+            val testList = if (document.exists()) document.toObject(PsTestDocumentResponse::class.java) else null
+            Result.Success(testList?.tests ?: emptyList())
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun getTest(id: String, abilityId: Int, abilityTaskId: Int): Result<PsTestResponse?> {
+        return try {
+            val documentId = "${abilityId}_${abilityTaskId}"
+            val document = getTestCollection().document(documentId).get().await()
+            val testList = if (document.exists()) document.toObject(PsTestDocumentResponse::class.java) else null
+            val test = testList?.tests?.find { it.id == id.toInt() }
+                Result.Success(test)
         } catch (e: Exception) {
             Result.Error(e)
         }
--- app/src/main/java/com/develofer/opositate/feature/test/domain/repository/TestRepository.kt
+++ app/src/main/java/com/develofer/opositate/feature/test/domain/repository/TestRepository.kt
@@ -1,10 +1,12 @@
 package com.develofer.opositate.feature.test.domain.repository
 
-import com.develofer.opositate.feature.profile.data.model.PsTest
-import com.develofer.opositate.feature.profile.presentation.model.PsTestVO
+import com.develofer.opositate.feature.profile.presentation.model.PsTestDocumentResponse
+import com.develofer.opositate.feature.profile.presentation.model.PsTestResponse
 import com.develofer.opositate.main.data.model.Result
 
 interface TestRepository {
-    suspend fun getTestList(): Result<List<PsTestVO>>
-    suspend fun getTest(id: String): Result<PsTest?>
+    suspend fun getTestList(): Result<List<PsTestDocumentResponse>>
+    suspend fun getTestListByAbility(abilityId: Int): Result<List<PsTestDocumentResponse>>
+    suspend fun getTestListByTask(abilityId: Int, abilityTaskId: Int): Result<List<PsTestResponse>>
+    suspend fun getTest(id: String, abilityId: Int, abilityTaskId: Int): Result<PsTestResponse?>
 }
