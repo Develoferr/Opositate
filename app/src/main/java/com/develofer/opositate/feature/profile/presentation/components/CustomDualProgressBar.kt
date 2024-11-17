114fc95679644be2d4513abc95a0be45f550b8ab
Fer <programacionfer@gmail.com>
2024-11-17 13:05:33 +0100

    Add rounded corners to CustomDualProgressBar

--- app/src/main/java/com/develofer/opositate/feature/profile/presentation/components/CustomDualProgressBar.kt
+++ app/src/main/java/com/develofer/opositate/feature/profile/presentation/components/CustomDualProgressBar.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,21 +31,21 @@ fun CustomDualProgressBar(
             progress = 1f,
             color = Gray400,
             modifier = Modifier
-                .height(20.dp)
+                .height(20.dp).clip(RoundedCornerShape(6.dp))
         )
 
         ProgressBar(
             progress = primaryProgress,
             color = Primary,
             modifier = Modifier
-                .height(20.dp)
+                .height(20.dp).clip(RoundedCornerShape(6.dp))
         )
 
         ProgressBar(
             progress = secondaryProgress,
             color = Secondary,
             modifier = Modifier
-                .height(20.dp)
+                .height(20.dp).clip(RoundedCornerShape(5.dp))
         )
     }
 }
