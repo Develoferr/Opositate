e423933982e91cfe326981f2dd33dcfc1efae734
Fer <programacionfer@gmail.com>
2024-12-02 00:41:21 +0100

    Update SolvedTestRepository for manage test types

--- app/src/main/java/com/develofer/opositate/feature/test/data/repository/SolvedTestRepositoryImpl.kt
+++ app/src/main/java/com/develofer/opositate/feature/test/data/repository/SolvedTestRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.develofer.opositate.feature.profile.data.model.TestResultDocument
 import com.develofer.opositate.feature.test.domain.repository.SolvedTestRepository
 import com.develofer.opositate.main.data.model.Result
 import com.develofer.opositate.main.data.provider.ResourceProvider
+import com.develofer.opositate.main.data.provider.TestType
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
@@ -23,30 +24,36 @@ class SolvedTestRepositoryImpl @Inject constructor(
         else {
             try {
                 val testResultList = if (isFirstTest) listOf(testResult)
-                else getTestResultList(userId)?.solvedTests?.plus(testResult) ?: listOf(testResult)
-                getSolvedTestCollection().document(userId).set(mapOf(resourceProvider.getString(R.string.firebase_constant__solved_tests) to testResultList)).await()
+                else getTestResultList(userId, testResult.testType)?.solvedTests?.plus(testResult) ?: listOf(testResult)
+                getSolvedTestCollection(testResult.testType).document(userId).set(mapOf(resourceProvider.getString(R.string.firebase_constant__solved_tests) to testResultList)).await()
                 Result.Success(Unit)
             } catch (e: Exception) {
                 Result.Error(e)
             }
         }
 
-    override suspend fun getTestResult(solvedTestId: String, userId: String): Result<TestResult?> =
+    override suspend fun getTestResult(solvedTestId: String, userId: String, testType: TestType): Result<TestResult?> =
         if (userId.isBlank()) Result.Error(Exception(resourceProvider.getString(R.string.error_message__user_not_authenticated)))
         else {
             try {
-                Result.Success(getTestResultList(userId)?.solvedTests?.find { it.id == solvedTestId })
+                Result.Success(getTestResultList(userId, testType)?.solvedTests?.find { it.id == solvedTestId })
             } catch (e: Exception) {
                 Result.Error(e)
             }
         }
 
 
-    private fun getSolvedTestCollection(): CollectionReference =
-        firestore.collection(resourceProvider.getString(R.string.firebase_constant__solved_tests))
+    private fun getSolvedTestCollection(testType: TestType): CollectionReference =
+        when (testType) {
+            TestType.ABILITY -> firestore.collection(resourceProvider.getString(R.string.firebase_constant__solved_tests_ability))
+            TestType.GENERAL -> firestore.collection(resourceProvider.getString(R.string.firebase_constant__solved_tests_general))
+            TestType.GROUP -> firestore.collection(resourceProvider.getString(R.string.firebase_constant__solved_tests_group))
+            TestType.TASK -> firestore.collection(resourceProvider.getString(R.string.firebase_constant__solved_tests_task))
+            TestType.CUSTOM -> firestore.collection(resourceProvider.getString(R.string.firebase_constant__solved_tests_custom))
+        }
 
-    private suspend fun getTestResultList(userId: String): TestResultDocument? {
-        val solvedTestDocument = getSolvedTestCollection().document(userId).get().await()
+    private suspend fun getTestResultList(userId: String, testType: TestType): TestResultDocument? {
+        val solvedTestDocument = getSolvedTestCollection(testType).document(userId).get().await()
         return if (solvedTestDocument.exists()) solvedTestDocument.toObject(TestResultDocument::class.java)
             else null
     }
--- app/src/main/java/com/develofer/opositate/feature/test/domain/repository/SolvedTestRepository.kt
+++ app/src/main/java/com/develofer/opositate/feature/test/domain/repository/SolvedTestRepository.kt
@@ -2,8 +2,9 @@ package com.develofer.opositate.feature.test.domain.repository
 
 import com.develofer.opositate.feature.profile.data.model.TestResult
 import com.develofer.opositate.main.data.model.Result
+import com.develofer.opositate.main.data.provider.TestType
 
 interface SolvedTestRepository {
     suspend fun saveTestResult(testResult: TestResult, userId: String, isFirstTest: Boolean): Result<Unit>
-    suspend fun getTestResult(solvedTestId: String, userId: String): Result<TestResult?>
+    suspend fun getTestResult(solvedTestId: String, userId: String, testType: TestType): Result<TestResult?>
 }
