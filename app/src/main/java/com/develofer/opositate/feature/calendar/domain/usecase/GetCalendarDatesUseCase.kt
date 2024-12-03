0f803870951f30f28a686dc531528ffbd906f2f9
Fer <programacionfer@gmail.com>
2024-12-03 23:54:12 +0100

    Update get calendar dates to support week start customization

--- app/src/main/java/com/develofer/opositate/feature/calendar/domain/usecase/GetCalendarDatesUseCase.kt
+++ app/src/main/java/com/develofer/opositate/feature/calendar/domain/usecase/GetCalendarDatesUseCase.kt
@@ -2,12 +2,13 @@ package com.develofer.opositate.feature.calendar.domain.usecase
 
 import com.develofer.opositate.feature.calendar.domain.repository.CalendarRepository
 import com.develofer.opositate.feature.calendar.presentation.model.CalendarUiState
+import com.develofer.opositate.feature.calendar.utils.WeekConfiguration
 import java.time.YearMonth
 import javax.inject.Inject
 
 class GetCalendarDatesUseCase @Inject constructor(
     private val repository: CalendarRepository
 ) {
-    operator fun invoke(yearMonth: YearMonth): List<CalendarUiState.Date> =
-        repository.getDates(yearMonth)
+    operator fun invoke(yearMonth: YearMonth, weekConfiguration: WeekConfiguration): List<CalendarUiState.Date> =
+        repository.getDates(yearMonth, weekConfiguration)
 }
