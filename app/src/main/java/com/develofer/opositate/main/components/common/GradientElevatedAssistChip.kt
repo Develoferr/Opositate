eea171fcf89e108d0eab87f6985f42e5aa7289bb
Fer <programacionfer@gmail.com>
2024-12-08 02:22:39 +0100

    Add theme support on GradientElevatedAssistChip

--- app/src/main/java/com/develofer/opositate/main/components/common/GradientElevatedAssistChip.kt
+++ app/src/main/java/com/develofer/opositate/main/components/common/GradientElevatedAssistChip.kt
@@ -13,18 +13,21 @@ import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.develofer.opositate.ui.theme.Gray900
 import com.develofer.opositate.ui.theme.Gray960
-
+import com.develofer.opositate.ui.theme.Secondary
 
 @Composable
 fun GradientElevatedAssistChip(
+    modifier: Modifier = Modifier,
     onClick: () -> Unit,
-    label: String,
+    label: String? = null,
     isSelected: Boolean,
-    modifier: Modifier = Modifier
+    isDarkTheme: Boolean,
+    leadingIcon: @Composable (() -> Unit)? = null
 ) {
     val colors = listOf(MaterialTheme.colorScheme.primary, MaterialTheme.colorScheme.secondary)
 
@@ -55,20 +58,38 @@ fun GradientElevatedAssistChip(
     )
 
     ElevatedAssistChip(
+        leadingIcon = leadingIcon,
         onClick = onClick,
         modifier = modifier,
         label = {
-            Text(
-                text = label.uppercase(),
-                fontSize = 10.sp,
-                modifier = textStyle,
-                color = if (isSelected) Color.Unspecified else MaterialTheme.colorScheme.onSurface
-            )
+            if (label != null) {
+                Text(
+                    text = label.uppercase(),
+                    fontSize = 10.sp,
+                    color = when {
+                        isDarkTheme && isSelected -> Color.Unspecified
+                        isDarkTheme && !isSelected -> MaterialTheme.colorScheme.onSurface
+                        !isDarkTheme && isSelected -> Color.White
+                        else -> Gray900
+                    },
+                    fontWeight = if (!isDarkTheme) FontWeight.W400 else FontWeight.Normal,
+                    modifier = if (isDarkTheme) textStyle else Modifier
+                )
+            }
         },
         colors = AssistChipDefaults.elevatedAssistChipColors(
-            containerColor = if (isSelected) Color.Black else Gray960
+            containerColor = when {
+                isDarkTheme && isSelected -> Color.Black
+                isDarkTheme && !isSelected -> Gray960
+                !isDarkTheme && isSelected -> Secondary
+                else -> Color.White
+            }
         ),
-        border = if (isSelected) BorderStroke(width = 2.dp, brush = borderBrush)
-        else BorderStroke(1.dp, Gray900)
+        border = when {
+            isDarkTheme && isSelected -> BorderStroke(width = 2.dp, brush = borderBrush)
+            isDarkTheme && !isSelected -> BorderStroke(1.dp, Gray900)
+            !isDarkTheme && !isSelected -> BorderStroke(2.dp, Secondary)
+            else -> BorderStroke(width = 0.dp, color = Color.Transparent)
+        }
     )
 }
